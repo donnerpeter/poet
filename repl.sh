f4697738cbe9f0ccef1effc96cddbe0e184bcc78
peter <peter@jetbrains.com>
2019-05-03 22:17:38 +0200

    cleanup cabal

--- repl.sh
+++ repl.sh
@@ -1,1 +1,4 @@
 stack ghci --ghci-options "-fobject-code -O2" | ./pretty.sh
+
+# to print Core
+# stack -v build poet:exe:poet-exe --ghc-options '-ddump-simpl -dsuppress-idinfo -dsuppress-coercions -dsuppress-type-applications -dsuppress-uniques -dsuppress-module-prefixes'
